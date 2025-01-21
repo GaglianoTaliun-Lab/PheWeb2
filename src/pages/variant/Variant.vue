a9c1f9512a2bb6922504337463737cb586297fa7
le-chang <le.chang@umontreal.ca>
2025-01-21 14:57:35 -0500

    Added phewas plot in the main branch and added table under the phewas plots

--- src/pages/variant/Variant.vue
+++ src/pages/variant/Variant.vue
@@ -1,69 +1,67 @@
 <script setup name="Variant">
-  import axios from 'axios';
-  import { ref, onMounted } from 'vue';
-  import { useRoute } from 'vue-router';
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
-  import {maf_range} from './Variant.js';
- 
-  import Navbar2 from '../../components/Navbar2.vue';
-  import PhewasPlot from '../../components/PhewasPlot.vue';
+import { maf_range } from './Variant.js';
 
-  // TODO: remove this eventually, should be global or only 38 allowed, or something.
-  var HG_BUILD_NUMBER = "38";
+import Navbar2 from '../../components/Navbar2.vue';
+import PhewasPlot from '../../components/PhewasPlot.vue';
 
-  const route = useRoute();
+// TODO: remove this eventually, should be global or only 38 allowed, or something.
+var HG_BUILD_NUMBER = "38";
 
-  const variantCode = route.params.variant_id;
-  const stratification_list = ref(null)
-  const variantList = ref([]); // TODO: fix this (self-made) mess... why are there two variantLists??
-  const maf_text = ref(null);
-  const variant = ref(null);
-  const rsids = ref(null);
-  const variant_list = ref([]);
-  const api = import.meta.env.VITE_APP_CLSA_PHEWEB_API_URL;
+const route = useRoute();
 
-  onMounted(async () => {
-    try {
-      const response = await axios.get(`${api}/variant/stratification_list`)
+const variantCode = route.params.variant_id;
+const stratification_list = ref(null);
+const variantList = ref([]); // TODO: fix this (self-made) mess... why are there two variantLists??
+const maf_text = ref(null);
+const variant = ref(null);
+const rsids = ref(null);
+const variant_list = ref([]);
+const api = import.meta.env.VITE_APP_CLSA_PHEWEB_API_URL;
 
-      stratification_list.value = JSON.parse(JSON.stringify(response.data));
+onMounted(async () => {
+  try {
+    const response = await axios.get(`${api}/variant/stratification_list`);
 
-      console.log(stratification_list.value)
+    stratification_list.value = JSON.parse(JSON.stringify(response.data));
 
-      // we need to map here to get rid of the proxy
-      variantList.value = await fetchPhewasPlottingData(stratification_list.value.map(stratification => stratification))
+    // we need to map here to get rid of the proxy
+    variantList.value = await fetchPhewasPlottingData(
+      stratification_list.value.map((stratification) => stratification)
+    );
 
-      maf_text.value = maf_range(variantList.value); // TODO only taking the first one here, should be some kind of combination of all stratifications
-      variant.value = variantList.value[0];
-      rsids.value = variant.value.rsids ? variant.value.rsids.split(',') : [];
-    }
-    catch (error) {
-      console.log(error);
-    }
-  });
-  
-  async function fetchPhewasPlottingData(stratification_list){
-    var temp_variant_list = []
-    for (var stratification of stratification_list){
-      var result;
-      try {
-          const response = await axios.get(`${api}/variant/${variantCode}/${stratification}`)
-          result = response.data
-          
-          result.stratification = "." + stratification.toLowerCase();
-          temp_variant_list.push(result)
-
-          console.log(result)
-      } catch (error) {
-        console.log(`Error fetching plotting data with stratification ${stratification}:`, error);
-      }
-    }
-    variant_list.value = temp_variant_list;
-    console.log(`VARIANT LIST : ${variant_list.value}`)
-
-    return(variant_list.value)
+    maf_text.value = maf_range(variantList.value); // TODO only taking the first one here, should be some kind of combination of all stratifications
+    variant.value = variantList.value[0];
+    rsids.value = variant.value.rsids ? variant.value.rsids.split(',') : [];
+  } catch (error) {
+    console.log(error);
   }
-  
+});
+
+async function fetchPhewasPlottingData(stratification_list) {
+  var temp_variant_list = [];
+  for (var stratification of stratification_list) {
+    var result;
+    try {
+      const response = await axios.get(
+        `${api}/variant/${variantCode}/${stratification}`
+      );
+      result = response.data;
+      result.stratification = '.' + stratification;
+      temp_variant_list.push(result);
+    } catch (error) {
+      console.log(
+        `Error fetching plotting data with stratification ${stratification}:`,
+        error
+      );
+    }
+  }
+  variant_list.value = temp_variant_list;
+  return variant_list.value;
+}
 </script>
 
 <template>
@@ -71,46 +69,102 @@
     <Navbar2 />
     <v-main>
       <div class="ml-4 mt-2">
-        <h1 class="mb-0">{{variantCode}}</h1>
+        <h1 class="mb-0">{{ variantCode }}</h1>
+        <!-- why does variant_list here work but not variantList ???-->
         <div v-if="variant">
-          <p class="mb-0"> Nearest genes: {{variant.nearest_genes}}</p>
-          <p class="mb-0"> {{maf_text}} </p>
+          <p class="mb-0"> Nearest genes: {{ variant.nearest_genes }}</p>
+          <p class="mb-0"> {{ maf_text }} </p>
           <p class="mb-0">
             View on
-            <a :href="`http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg${HG_BUILD_NUMBER}&highlight=hg${HG_BUILD_NUMBER}.chr${variant.chrom}%3A${variant.pos}-${variant.pos}&position=chr${variant.chrom}%3A${variant.pos - 200000}-${variant.pos + 200000}`"
-              target="_blank" rel="noopener noreferrer">UCSC</a>,
-            <a :href="`https://www.ncbi.nlm.nih.gov/snp/?term=${variant.chrom}%5BChromosome%5D+AND+${variant.pos}%5BCHRPOS%5D`"
-              target="_blank" rel="noopener noreferrer">dbSNP</a>,
-            <a :href="`https://useast.ensembl.org/Homo_sapiens/Location/Variant/Table?r=${variant.chrom}:${variant.pos}-${variant.pos}`"
-              target="_blank" rel="noopener noreferrer">Ensembl</a>,
-            <a :href="`https://genetics.opentargets.org/Variant/${variant.chrom}_${variant.pos}_${variant.ref}_${variant.alt}/associations`"
-              target="_blank" rel="noopener noreferrer">Open Targets Genetics</a>,
-            <a :href="`https://gnomad.broadinstitute.org/variant/${variant.chrom}-${variant.pos}-${variant.ref}-${variant.alt}?dataset=gnomad_r4`"
-              target="_blank" rel="noopener noreferrer">gnomAD</a>
+            <a
+              :href="`http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg${HG_BUILD_NUMBER}&highlight=hg${HG_BUILD_NUMBER}.chr${variant.chrom}%3A${variant.pos}-${variant.pos}&position=chr${variant.chrom}%3A${variant.pos - 200000}-${variant.pos + 200000}`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >UCSC</a
+            >,
+            <a
+              :href="`https://www.ncbi.nlm.nih.gov/snp/?term=${variant.chrom}%5BChromosome%5D+AND+${variant.pos}%5BCHRPOS%5D`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >dbSNP</a
+            >,
+            <a
+              :href="`https://useast.ensembl.org/Homo_sapiens/Location/Variant/Table?r=${variant.chrom}:${variant.pos}-${variant.pos}`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Ensembl</a
+            >,
+            <a
+              :href="`https://genetics.opentargets.org/Variant/${variant.chrom}_${variant.pos}_${variant.ref}_${variant.alt}/associations`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Open Targets Genetics</a
+            >,
+            <a
+              :href="`https://gnomad.broadinstitute.org/variant/${variant.chrom}-${variant.pos}-${variant.ref}-${variant.alt}?dataset=gnomad_r4`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >gnomAD</a
+            >
 
             <template v-if="rsids.length === 1">
-              , <a :href="`https://www.ebi.ac.uk/gwas/search?query=${rsids[0]}`">GWAS Catalog</a>
-              , <a
-                :href="`http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?searchType=adhoc_search&type=rs&rs=${rsids[0]}`">dbSNP</a>
+              , <a :href="`https://www.ebi.ac.uk/gwas/search?query=${rsids[0]}`"
+                >GWAS Catalog</a
+              >,
+              <a
+                :href="`http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?searchType=adhoc_search&type=rs&rs=${rsids[0]}`"
+                >dbSNP</a
+              >
             </template>
 
             <template v-else>
               <span v-for="rsid in rsids" :key="rsid">
-                , <a :href="`https://www.ebi.ac.uk/gwas/search?query=${rsid}`">GWAS Catalog for {{ rsid }}</a>
-                , <a
-                  :href="`http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?searchType=adhoc_search&type=rs&rs=${rsid}`">dbSNP
-                  for {{ rsid }}</a>
+                , <a :href="`https://www.ebi.ac.uk/gwas/search?query=${rsid}`"
+                  >GWAS Catalog for {{ rsid }}</a
+                >,
+                <a
+                  :href="`http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?searchType=adhoc_search&type=rs&rs=${rsid}`"
+                  >dbSNP for {{ rsid }}</a
+                >
               </span>
             </template>
-            <span style="font-weight:bold" id="pubmed-link"></span>
-            <span style="font-weight:bold" id="clinvar-link"></span>
+            <span style="font-weight: bold" id="pubmed-link"></span>
+            <span style="font-weight: bold" id="clinvar-link"></span>
           </p>
         </div>
         <div v-if="variant_list.length > 0">
           <PhewasPlot :variantList="variant_list" />
         </div>
+
+        <!-- Add a table below the plots -->
+        <div v-if="variant_list.length > 0" class="mt-4">
+          <table class="table-auto border-collapse border border-gray-400 w-full text-left">
+            <thead>
+              <tr class="bg-gray-200">
+                <th class="border border-gray-400 px-4 py-2">Category</th>
+                <th class="border border-gray-400 px-4 py-2">Phenotype</th>
+                <th class="border border-gray-400 px-4 py-2">P-value</th>
+                <th class="border border-gray-400 px-4 py-2">Effect Size (se)</th>
+                <th class="border border-gray-400 px-4 py-2">Number of Samples</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(pheno, index) in variant_list.flatMap((v) => v.phenos)"
+                :key="index"
+              >
+                <td class="border border-gray-400 px-4 py-2">{{ pheno.category }}</td>
+                <td class="border border-gray-400 px-4 py-2">{{ pheno.phenostring }}</td>
+                <td class="border border-gray-400 px-4 py-2">{{ pheno.pval }}</td>
+                <td class="border border-gray-400 px-4 py-2">
+                  {{ pheno.beta }} ({{ pheno.sebeta }})
+                </td>
+                <td class="border border-gray-400 px-4 py-2">{{ pheno.num_samples }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </v-main>
   </v-app>
-
 </template>
