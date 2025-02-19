b71d81edd9bc7c0df9efff9b45f49ed993098535
JustinBellavance <justin.bellavance@gmail.com>
2025-02-19 15:33:16 -0500

    merge

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //TODO: put all stratifications in order of priority
-export const PRIORITY_STRATIFICATIONS = ["European.Male", "European.Female"]
+export const PRIORITY_STRATIFICATIONS = ["european.male", "european.female"]
 export const HG_BUILD_NUMBER = "38";
