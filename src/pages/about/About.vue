7e0dae5af6851051c269dc7ee05bf4467af38a79
Seyla Wickramasinghe <wickens@narval3.narval.calcul.quebec>
2025-05-16 16:13:39 -0400

    Modified PCA plots for About page

--- src/pages/about/About.vue
+++ src/pages/about/About.vue
@@ -146,9 +146,14 @@
                         <br/>
                         <!-- Insert the image here -->
                         <v-card>
-                            <b>Figure 1.</b>CLSA sample's with inferred European ancestry projected onto the HGDP and 1KG's PCA space.
-                            <img src="../../assets/pca.png" alt="Figure 1 - PCA Projection" class="img-fluid mt-3" />
+                            <img src="../../assets/eur_vs_reference_square_pcs.png" alt="Figure 1 - PCA Projection" class="img-fluid mt-3" />
+                            <b>Figure 1.</b>CLSA samples with inferred European ancestry projected onto the HGDP and 1KG's PCA space.
                         </v-card>
+                        <v-card>
+                            <img src="../../assets/incrfont_top3pcs_eur_subset_square_title.png" alt="Figure 2 - Top 3 Principal Components" class="img-fluid mt-3" />
+                            <b>Figure 2.</b>Top 3 principal components of CLSA samples with inferred European ancestry (N=24505).
+                        </v-card>
+                        
                     </div>
 
                     <div v-if="selectedContent === 'BinaryPhenotype'" id = 'BinaryPhenotype'>
